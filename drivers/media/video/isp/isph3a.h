fee71bba048e81e93219fa33ab15792479084414
Mohit Jalori <mjalori@ti.com>
2008-07-25 16:23:17 -0500

    ARM: OMAP: Moving ISP structs to a file under include directory.
    This patch moves ISP structs to a file under include directory.
    
    Signed-off-by: Mohit Jalori <mjalori@ti.com>

--- drivers/media/video/isp/isph3a.h
+++ drivers/media/video/isp/isph3a.h
@@ -17,6 +17,8 @@
 #ifndef OMAP_ISP_H3A_H
 #define OMAP_ISP_H3A_H
 
+#include <asm/arch/isp_user.h>
+
 #define AEWB_PACKET_SIZE	16
 #define H3A_MAX_BUFF		5
 
@@ -47,8 +49,6 @@
 #define MIN_SUB_INC		2
 #define MAX_SUB_INC		32
 
-#define MAX_FRAME_COUNT		0x0FFF
-#define MAX_FUTURE_FRAMES	10
 
 /* ISPH3A REGISTERS bits */
 #define ISPH3A_PCR_AF_EN	(1 << 0)
@@ -107,76 +107,6 @@
 #define WRITE_SUB_HOR_INC(reg, sub_hor_inc) \
 		(reg = (reg & ~(ISPH3A_AEWSUBWIN_AEWINCH_MASK)) \
 		| (((sub_hor_inc >> 1) - 1) << ISPH3A_AEWSUBWIN_AEWINCH_SHIFT))
-
-/**
- * struct isph3a_aewb_config - AE AWB configuration reset values.
- * saturation_limit: Saturation limit.
- * @win_height: Window Height. Range 2 - 256, even values only.
- * @win_width: Window Width. Range 6 - 256, even values only.
- * @ver_win_count: Vertical Window Count. Range 1 - 128.
- * @hor_win_count: Horizontal Window Count. Range 1 - 36.
- * @ver_win_start: Vertical Window Start. Range 0 - 4095.
- * @hor_win_start: Horizontal Window Start. Range 0 - 4095.
- * @blk_ver_win_start: Black Vertical Windows Start. Range 0 - 4095.
- * @blk_win_height: Black Window Height. Range 2 - 256, even values only.
- * @subsample_ver_inc: Subsample Vertical points increment Range 2 - 32, even
- *                     values only.
- * @subsample_hor_inc: Subsample Horizontal points increment Range 2 - 32, even
- *                     values only.
- * @alaw_enable: AEW ALAW EN flag.
- * @aewb_enable: AE AWB stats generation EN flag.
- */
-struct isph3a_aewb_config {
-	u16 saturation_limit;
-	u16 win_height;
-	u16 win_width;
-	u16 ver_win_count;
-	u16 hor_win_count;
-	u16 ver_win_start;
-	u16 hor_win_start;
-	u16 blk_ver_win_start;
-	u16 blk_win_height;
-	u16 subsample_ver_inc;
-	u16 subsample_hor_inc;
-	u8 alaw_enable;
-	u8 aewb_enable;
-};
-
-/**
- * struct isph3a_aewb_data - Structure of data sent to or received from user
- * @h3a_aewb_statistics_buf: Pointer to pass to user.
- * @shutter: Shutter speed.
- * @gain: Sensor analog Gain.
- * @shutter_cap: Shutter speed for capture.
- * @gain_cap: Sensor Gain for capture.
- * @dgain: White balance digital gain.
- * @wb_gain_b: White balance color gain blue.
- * @wb_gain_r: White balance color gain red.
- * @wb_gain_gb: White balance color gain green blue.
- * @wb_gain_gr: White balance color gain green red.
- * @frame_number: Frame number of requested stats.
- * @curr_frame: Current frame number being processed.
- * @update: Bitwise flags to update parameters.
- * @ts: Timestamp of returned framestats.
- * @field_count: Sequence number of returned framestats.
- */
-struct isph3a_aewb_data {
-	void *h3a_aewb_statistics_buf;
-	u32 shutter;
-	u16 gain;
-	u32 shutter_cap;
-	u16 gain_cap;
-	u16 dgain;
-	u16 wb_gain_b;
-	u16 wb_gain_r;
-	u16 wb_gain_gb;
-	u16 wb_gain_gr;
-	u16 frame_number;
-	u16 curr_frame;
-	u8 update;
-	struct timeval ts;
-	unsigned long field_count;
-};
 
 /**
  * struct isph3a_aewb_xtrastats - Structure with extra statistics sent by cam.
