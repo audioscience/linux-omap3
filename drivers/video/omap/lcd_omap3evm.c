35c617541701ca738277278f282c1de83c019449
Sanjeev Premi <premi@ti.com>
2009-01-16 16:27:33 +0530

    Backlight driver for omap3evm
    This patch implements the backlight driver for OMAP3EVM.
    The mechanism for backlight control is reused from the
    existing controls in lcd_omap3evm.c.

--- drivers/video/omap/lcd_omap3evm.c
+++ drivers/video/omap/lcd_omap3evm.c
@@ -49,8 +49,6 @@
 #define TWL_PWMA_PWMAON		0x00
 #define TWL_PWMA_PWMAOFF	0x01
 
-static unsigned int bklight_level;
-
 static int omap3evm_panel_init(struct lcd_panel *panel,
 				struct omapfb_device *fbdev)
 {
@@ -69,7 +67,6 @@ static int omap3evm_panel_init(struct lcd_panel *panel,
 	twl4030_i2c_write_u8(TWL4030_MODULE_LED, 0x11, TWL_LED_LEDEN);
 	twl4030_i2c_write_u8(TWL4030_MODULE_PWMA, 0x01, TWL_PWMA_PWMAON);
 	twl4030_i2c_write_u8(TWL4030_MODULE_PWMA, 0x02, TWL_PWMA_PWMAOFF);
-	bklight_level = 100;
 
 	return 0;
 }
@@ -92,28 +89,6 @@ static void omap3evm_panel_disable(struct lcd_panel *panel)
 static unsigned long omap3evm_panel_get_caps(struct lcd_panel *panel)
 {
 	return 0;
-}
-
-static int omap3evm_bklight_setlevel(struct lcd_panel *panel,
-						unsigned int level)
-{
-	u8 c;
-	if ((level >= 0) && (level <= 100)) {
-		c = (125 * (100 - level)) / 100 + 2;
-		twl4030_i2c_write_u8(TWL4030_MODULE_PWMA, c, TWL_PWMA_PWMAOFF);
-		bklight_level = level;
-	}
-	return 0;
-}
-
-static unsigned int omap3evm_bklight_getlevel(struct lcd_panel *panel)
-{
-	return bklight_level;
-}
-
-static unsigned int omap3evm_bklight_getmaxlevel(struct lcd_panel *panel)
-{
-	return 100;
 }
 
 struct lcd_panel omap3evm_panel = {
@@ -139,9 +114,6 @@ struct lcd_panel omap3evm_panel = {
 	.enable		= omap3evm_panel_enable,
 	.disable	= omap3evm_panel_disable,
 	.get_caps	= omap3evm_panel_get_caps,
-	.set_bklight_level      = omap3evm_bklight_setlevel,
-	.get_bklight_level      = omap3evm_bklight_getlevel,
-	.get_bklight_max        = omap3evm_bklight_getmaxlevel,
 };
 
 static int omap3evm_panel_probe(struct platform_device *pdev)
