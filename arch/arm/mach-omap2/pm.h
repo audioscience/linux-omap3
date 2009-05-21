ae22fe3672bcaf1dffeb06fb768c055e7d08797e
Mike Chan <mike@android.com>
2009-05-21 16:47:11 -0700

    OMAP3: PM: Add voltage levels to prm_setup_vc (previously prm_setup_time)
    Expand configure_vc() to support configurable voltage levels.
    
    Signed-off-by: Mike Chan <mike@android.com>

--- arch/arm/mach-omap2/pm.h
+++ arch/arm/mach-omap2/pm.h
@@ -17,19 +17,29 @@ extern void *omap3_secure_ram_storage;
 extern unsigned short enable_off_mode;
 extern unsigned short voltage_off_while_idle;
 
-struct prm_setup_times {
+struct prm_setup_vc {
 	u16 clksetup;
 	u16 voltsetup_time1;
 	u16 voltsetup_time2;
 	u16 voltoffset;
 	u16 voltsetup2;
+/* PRM_VC_CMD_VAL_0 specific bits */
+	u16 vdd0_on;
+	u16 vdd0_onlp;
+	u16 vdd0_ret;
+	u16 vdd0_off;
+/* PRM_VC_CMD_VAL_1 specific bits */
+	u16 vdd1_on;
+	u16 vdd1_onlp;
+	u16 vdd1_ret;
+	u16 vdd1_off;
 };
 
 extern void omap3_pm_off_mode_enable(int);
 extern void omap_sram_idle(void);
 extern int omap3_can_sleep(void);
 extern int set_pwrdm_state(struct powerdomain *pwrdm, u32 state);
-extern void omap3_set_prm_setup_times(struct prm_setup_times *setup_times);
+extern void omap3_set_prm_setup_vc(struct prm_setup_vc *setup_vc);
 
 #ifdef CONFIG_CPU_IDLE
 int omap3_idle_init(void);
