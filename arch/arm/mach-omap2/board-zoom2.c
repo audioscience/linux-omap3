af3038b43f8c675cee3f5ffab11c6be126c955ae
Kevin Hilman <khilman@deeprootsystems.com>
2009-06-17 10:49:59 -0700

    OMAP3: Zoom2: add support for default OPPs

--- arch/arm/mach-omap2/board-zoom2.c
+++ arch/arm/mach-omap2/board-zoom2.c
@@ -26,6 +26,7 @@
 
 #include "mmc-twl4030.h"
 #include "sdram-micron-mt46h32m32lf-6.h"
+#include "omap3-opp.h"
 
 /* Zoom2 has Qwerty keyboard*/
 static int board_keymap[] = {
@@ -215,7 +216,8 @@ static void __init omap_zoom2_init_irq(void)
 	omap_board_config = zoom2_config;
 	omap_board_config_size = ARRAY_SIZE(zoom2_config);
 	omap2_init_common_hw(mt46h32m32lf6_sdrc_params,
-				 mt46h32m32lf6_sdrc_params);
+				 mt46h32m32lf6_sdrc_params, omap3_mpu_rate_table,
+				 omap3_dsp_rate_table, omap3_l3_rate_table);
 	omap_init_irq();
 	omap_gpio_init();
 }
