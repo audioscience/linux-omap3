4d2ec86bdfbf1f5ac9603a8c7467461503910ce5
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-07-01 12:56:03 +0530

    cppi41: Ravi's patch to fix teardown path
    - Need to review and update the patch content

--- drivers/usb/musb/cppi41.c
+++ drivers/usb/musb/cppi41.c
@@ -255,6 +255,7 @@ int __init cppi41_queue_mgr_init(struct musb *musb, u8 q_mgr, dma_addr_t rgn0_ba
 
 	return 0;
 }
+EXPORT_SYMBOL(cppi41_queue_mgr_init);
 
 int cppi41_dma_sched_tbl_init(u8 dma_num, u8 q_mgr,
 				 u32 *sched_tbl, u8 tbl_size)
@@ -262,7 +263,7 @@ int cppi41_dma_sched_tbl_init(u8 dma_num, u8 q_mgr,
 	struct cppi41_dma_block *dma_block;
 	int num_reg, k, i,val=0;
 
-	dma_block = &cppi41_dma_block[dma_num];
+	dma_block = (struct cppi41_dma_block *)&cppi41_dma_block[dma_num];
 
 	num_reg = (tbl_size + 3) / 4;
         for (k = i = 0; i < num_reg; i++) {
@@ -280,9 +281,10 @@ int cppi41_dma_sched_tbl_init(u8 dma_num, u8 q_mgr,
                   //  dma_block->sched_table_base + DMA_SCHED_TABLE_WORD_REG(i),
                     //val);
         }
-
+	return 0;
 }
 EXPORT_SYMBOL(cppi41_dma_sched_tbl_init);
+
 int __init cppi41_dma_block_init(u8 dma_num, u8 q_mgr, u8 num_order,
 				 u32 *sched_tbl, u8 tbl_size)
 {
@@ -405,7 +407,7 @@ free_queue:
 	cppi41_queue_free(q_mgr, q_num);
 	return error;
 }
-
+EXPORT_SYMBOL(cppi41_dma_block_init);
 /*
  * cppi41_mem_rgn_alloc - allocate a memory region within the queue manager
  */
@@ -744,6 +746,17 @@ void cppi41_dma_ch_disable(struct cppi41_dma_ch_obj *dma_ch_obj)
 	    __raw_readl(dma_ch_obj->base_addr));
 }
 EXPORT_SYMBOL(cppi41_dma_ch_disable);
+
+void cppi41_free_teardown_queue(int dma_num)
+{
+       unsigned long td_addr;
+
+       while( (td_addr= cppi41_queue_pop(&dma_teardown[dma_num].queue_obj)) != 0) {
+               DBG1("pop tdDesc(%p) from tdQueue\n", td_addr);
+	}
+
+}
+EXPORT_SYMBOL(cppi41_free_teardown_queue);
 
 /**
  * alloc_queue - allocate a queue in the given range
