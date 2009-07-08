5e5c96479a07db38767c1a16e4127e3a5476ad50
Rajendra Nayak <rnayak@ti.com>
2009-07-08 10:25:37 -0700

    OMAP3: PM: move context-loss counting into OMAP PM
    Drop get_last_off_on_transaction_id() and move functionality
    into OMAP PM layer.
    
    For SRF, use omapdev** to get powerdomain state counte
    for NOOP, use an increasing counter to ensure context is always restord.
    
    **NOTE: in future PM branch, this omapdev functinality will be replaced
            by omap_hwmod.  Thus, context loss counting will only exist
            for modules with omap_hwmod implementations.
    
    Cc: Peter 'p2' De Schrijver <peter.de-schrijver@nokia.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -31,7 +31,6 @@
 #include <asm/atomic.h>
 
 #include <plat/powerdomain.h>
-#include <plat/omapdev.h>
 #include <plat/resource.h>
 #include <plat/omap34xx.h>
 
@@ -168,21 +167,6 @@ void omap2_allow_sleep(void)
 
 	i = atomic_dec_return(&sleep_block);
 	BUG_ON(i < 0);
-}
-
-unsigned get_last_off_on_transaction_id(struct device *dev)
-{
-	struct platform_device *pdev = to_platform_device(dev);
-	struct omapdev *odev = omapdev_find_pdev(pdev);
-	struct powerdomain *pwrdm;
-
-	if (odev) {
-		pwrdm = omapdev_get_pwrdm(odev);
-		if (pwrdm)
-			return pwrdm->state_counter[0];
-	}
-
-	return 0;
 }
 
 static int __init omap_pm_init(void)
