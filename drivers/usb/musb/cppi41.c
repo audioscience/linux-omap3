7aff360081e613fee5de9ae9be996a1bd0bbb274
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-07-28 19:54:15 +0530

    cppi41: fix moduler build for musb driver
    musb driver calls queue manager init and dma block init functions
    and so these functions has to be exported for moduler build of
    musb controller driver.
    
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

--- drivers/usb/musb/cppi41.c
+++ drivers/usb/musb/cppi41.c
@@ -110,6 +110,7 @@ int __init cppi41_queue_mgr_init(u8 q_mgr, dma_addr_t rgn0_base, u16 rgn0_size)
 
 	return 0;
 }
+EXPORT_SYMBOL(cppi41_queue_mgr_init);
 
 int __init cppi41_dma_block_init(u8 dma_num, u8 q_mgr, u8 num_order,
 				 u8 *sched_tbl, u8 tbl_size)
@@ -231,6 +232,7 @@ free_queue:
 	cppi41_queue_free(q_mgr, q_num);
 	return error;
 }
+EXPORT_SYMBOL(cppi41_dma_block_init);
 
 /*
  * cppi41_mem_rgn_alloc - allocate a memory region within the queue manager
