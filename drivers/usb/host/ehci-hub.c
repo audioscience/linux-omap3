1a1a9621baf40cf62fde70a2fc245ebfeeadfe95
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-08-18 17:33:01 +0530

    usb: ehci: fix companion port ownership issue
    We face below issue without this fix:
    	- EHCI unusable (dead) when a FS device is connected
    	  directly to EHCI port.
    	- EHCI unusable (dead) when a USB pendrive is removed
    	  while data is being written onto it.

--- drivers/usb/host/ehci-hub.c
+++ drivers/usb/host/ehci-hub.c
@@ -430,7 +430,9 @@ static int check_reset_complete (
 			index + 1);
 
 		// what happens if HCS_N_CC(params) == 0 ?
+#if !defined(CONFIG_MACH_OMAP3EVM) && !defined(CONFIG_MACH_OMAP3517EVM)
 		port_status |= PORT_OWNER;
+#endif
 		port_status &= ~PORT_RWC_BITS;
 		ehci_writel(ehci, port_status, status_reg);
 
@@ -852,7 +854,9 @@ static int ehci_hub_control (
 				ehci_dbg (ehci,
 					"port %d low speed --> companion\n",
 					wIndex + 1);
+#if !defined(CONFIG_MACH_OMAP3EVM) && !defined(CONFIG_MACH_OMAP3517EVM)
 				temp |= PORT_OWNER;
+#endif
 			} else {
 				ehci_vdbg (ehci, "port %d reset\n", wIndex + 1);
 				temp |= PORT_RESET;
