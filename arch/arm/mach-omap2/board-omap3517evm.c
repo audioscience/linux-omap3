08db021cdaf0e11a509a65e2f69feb998d61331e
Vaibhav Hiremath <hvaibhav@ti.com>
2009-09-11 19:03:42 +0530

    OMAP3517: Enable DSS2 for OMAP3517EVM board
    TODO -
    	- DVI out testing

--- arch/arm/mach-omap2/board-omap3517evm.c
+++ arch/arm/mach-omap2/board-omap3517evm.c
@@ -37,6 +37,7 @@
 #include <mach/usb.h>
 #include <mach/common.h>
 #include <mach/mcspi.h>
+#include <mach/display.h>
 
 /*
  * Ethernet
@@ -70,6 +71,174 @@ static int __init omap3517_evm_i2c_init(void)
 /*
  * MTD
  */
+/*
+ * DSS
+ */
+/*
+ * Please note that USB2 port pins (D3 & D6) are muxed with
+ * LCD panel Backlight enable and PWM signals, making them mutual exclusive
+ * to each other.
+ */
+#define LCD_PANEL_PWR		176
+#define LCD_PANEL_BKLIGHT_PWR	182
+#define LCD_PANEL_PWM		181
+
+static int lcd_enabled;
+static int dvi_enabled;
+
+static void __init omap3517_evm_display_init(void)
+{
+#if defined(CONFIG_PANEL_SHARP_LQ043T1DG01) || \
+	defined(CONFIG_PANEL_SHARP_LQ043T1DG01_MODULE)
+	int r;
+
+	omap_cfg_reg(AA16_34XX_GPI181);
+	omap_cfg_reg(AE17_34XX_GPIO182);
+	omap_cfg_reg(AE15_34XX_GPIO176);
+	/*
+	 * Enable GPIO 182 = LCD Backlight Power
+	 */
+	r = gpio_request(LCD_PANEL_BKLIGHT_PWR, "lcd_backlight_pwr");
+	if (r) {
+		printk(KERN_ERR "failed to get LCD_PANEL_BKLIGHT_PWR\n");
+		return;
+	}
+	gpio_direction_output(LCD_PANEL_BKLIGHT_PWR, 1);
+
+	/*
+	 * Enable GPIO 181 = LCD Panel PWM
+	 *
+	 * We are not supporting configuration range for backlight
+	 * driver, so make it high (by default).
+	 *
+	 */
+	r = gpio_request(LCD_PANEL_PWM, "lcd_pwm");
+	if (r) {
+		printk(KERN_ERR "failed to get LCD_PANEL_PWM\n");
+		goto err_1;
+	}
+	gpio_direction_output(LCD_PANEL_PWM, 1);
+
+	/*
+	 * Enable GPIO 176 = LCD Panel Power enable pin
+	 */
+	r = gpio_request(LCD_PANEL_PWR, "lcd_panel_pwr");
+	if (r) {
+		printk(KERN_ERR "failed to get LCD_PANEL_PWR\n");
+		goto err_2;
+	}
+	gpio_direction_output(LCD_PANEL_PWR, 1);
+
+	printk(KERN_INFO "Display initialized successfully\n");
+	return;
+
+err_2:
+	gpio_free(LCD_PANEL_PWM);
+err_1:
+	gpio_free(LCD_PANEL_BKLIGHT_PWR);
+#endif
+}
+
+static int omap3517_evm_panel_enable_lcd(struct omap_dss_device *dssdev)
+{
+	if (dvi_enabled) {
+		printk(KERN_ERR "cannot enable LCD, DVI is enabled\n");
+		return -EINVAL;
+	}
+	gpio_direction_output(LCD_PANEL_PWR, 1);
+	lcd_enabled = 1;
+	return 0;
+}
+
+static void omap3517_evm_panel_disable_lcd(struct omap_dss_device *dssdev)
+{
+	gpio_direction_output(LCD_PANEL_PWR, 0);
+	lcd_enabled = 0;
+}
+
+/*
+ * TODO: Need to change depending on LCD Panel being used
+ */
+static struct omap_dss_device omap3517_evm_lcd_device = {
+	.type			= OMAP_DISPLAY_TYPE_DPI,
+	.name			= "lcd",
+	.driver_name		= "sharp_lq_panel",
+	.phy.dpi.data_lines 	= 16,
+	.platform_enable	= omap3517_evm_panel_enable_lcd,
+	.platform_disable	= omap3517_evm_panel_disable_lcd,
+};
+
+/*
+ * TV Out: Only used for OMAP3517TEB Board
+ */
+static int omap3517_evm_panel_enable_tv(struct omap_dss_device *dssdev)
+{
+	return 0;
+}
+
+static void omap3517_evm_panel_disable_tv(struct omap_dss_device *dssdev)
+{
+}
+
+static struct omap_dss_device omap3517_evm_tv_device = {
+	.type 			= OMAP_DISPLAY_TYPE_VENC,
+	.name 			= "tv",
+	.driver_name		= "venc",
+	.phy.venc.type		= OMAP_DSS_VENC_TYPE_SVIDEO,
+	.platform_enable	= omap3517_evm_panel_enable_tv,
+	.platform_disable	= omap3517_evm_panel_disable_tv,
+};
+
+
+static int omap3517_evm_panel_enable_dvi(struct omap_dss_device *dssdev)
+{
+	if (lcd_enabled) {
+		printk(KERN_ERR "cannot enable DVI, LCD is enabled\n");
+		return -EINVAL;
+	}
+	dvi_enabled = 1;
+
+	return 0;
+}
+
+static void omap3517_evm_panel_disable_dvi(struct omap_dss_device *dssdev)
+{
+	dvi_enabled = 0;
+}
+
+static struct omap_dss_device omap3517_evm_dvi_device = {
+	.type			= OMAP_DISPLAY_TYPE_DPI,
+	.name			= "dvi",
+	.driver_name		= "panel-generic",
+	.phy.dpi.data_lines	= 16,
+	.platform_enable	= omap3517_evm_panel_enable_dvi,
+	.platform_disable	= omap3517_evm_panel_disable_dvi,
+};
+
+static struct omap_dss_device *omap3517_evm_dss_devices[] = {
+	&omap3517_evm_lcd_device,
+	&omap3517_evm_tv_device,
+	&omap3517_evm_dvi_device,
+};
+
+static struct omap_dss_board_info omap3517_evm_dss_data = {
+	.num_devices	= ARRAY_SIZE(omap3517_evm_dss_devices),
+	.devices	= omap3517_evm_dss_devices,
+#if defined(CONFIG_PANEL_SHARP_LQ043T1DG01) || \
+		defined(CONFIG_PANEL_SHARP_LQ043T1DG01_MODULE)
+	.default_device	= &omap3517_evm_lcd_device,
+#else
+	.default_device = &omap3517_evm_tv_device,
+#endif
+};
+
+static struct platform_device omap3517_evm_dss_device = {
+	.name		= "omapdss",
+	.id		= -1,
+	.dev		= {
+		.platform_data	= &omap3517_evm_dss_data,
+	},
+};
 
 /*
  * Board initialization
@@ -78,6 +247,7 @@ static struct omap_board_config_kernel omap3517_evm_config[] __initdata = {
 };
 
 static struct platform_device *omap3517_evm_devices[] __initdata = {
+	&omap3517_evm_dss_device,
 };
 
 static void __init omap3517_evm_init_irq(void)
@@ -93,7 +263,12 @@ static void __init omap3517_evm_init_irq(void)
 static struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
 
 	.port_mode[0] = EHCI_HCD_OMAP_MODE_PHY,
+#if defined(CONFIG_PANEL_SHARP_LQ043T1DG01) || \
+		defined(CONFIG_PANEL_SHARP_LQ043T1DG01_MODULE)
+	.port_mode[1] = EHCI_HCD_OMAP_MODE_UNKNOWN,
+#else
 	.port_mode[1] = EHCI_HCD_OMAP_MODE_PHY,
+#endif
 	.port_mode[2] = EHCI_HCD_OMAP_MODE_UNKNOWN,
 
 	.chargepump = false,
@@ -117,6 +292,8 @@ static void __init omap3517_evm_init(void)
 	/* Setup EHCI phy reset padconfig for port1 using GPIO57 */
 	omap_cfg_reg(N5_3517_GPIO57_OUT);
 	usb_ehci_init(&ehci_pdata);
+
+	omap3517_evm_display_init();
 }
 
 static void __init omap3517_evm_map_io(void)
