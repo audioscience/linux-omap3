22975f488ed2d73fa838e332e4fcb081907c3b3b
Vaibhav Hiremath <hvaibhav@ti.com>
2009-09-12 23:42:40 +0530

    OMAP3CAM: Camera support (Regulator) to OMAp3EVM board file
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- arch/arm/mach-omap2/board-omap3evm.c
+++ arch/arm/mach-omap2/board-omap3evm.c
@@ -26,6 +26,7 @@
 #include <linux/spi/spi.h>
 #include <linux/spi/ads7846.h>
 #include <linux/i2c/twl4030.h>
+#include <linux/regulator/machine.h>
 #include <linux/usb/otg.h>
 #include <linux/smsc911x.h>
 
@@ -48,6 +49,7 @@
 #include "mmc-twl4030.h"
 #include "pm.h"
 #include "omap3-opp.h"
+#include "board-omap3evm-camera.h"
 
 #define OMAP3_EVM_TS_GPIO	175
 
@@ -310,6 +312,40 @@ static struct platform_device omap3_evm_dss_device = {
 	},
 };
 
+static struct platform_device omap3evm_camkit_device = {
+	.name		= "omap3evm_camkit",
+	.id		= -1,
+};
+static struct regulator_consumer_supply omap3evm_vaux2_supplies[] = {
+	{
+		.supply		= "vaux2_1",
+		.dev		= &omap3evm_camkit_device.dev,
+	},
+	{
+		.supply		= "vaux2_2",
+		.dev		= &omap3evm_camkit_device.dev,
+	},
+	{
+		.supply		= "vaux2_3",
+		.dev		= &omap3evm_camkit_device.dev,
+	},
+};
+
+/* VAUX2 for camera module */
+static struct regulator_init_data omap3evm_vaux2 = {
+	.constraints = {
+		.min_uV		= 2800000,
+		.max_uV		= 2800000,
+		.apply_uV	= true,
+		.valid_modes_mask	= REGULATOR_MODE_NORMAL
+					| REGULATOR_MODE_STANDBY,
+		.valid_ops_mask		= REGULATOR_CHANGE_MODE
+					| REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies		= ARRAY_SIZE(omap3evm_vaux2_supplies),
+	.consumer_supplies		= omap3evm_vaux2_supplies,
+};
+
 static struct twl4030_hsmmc_info mmc[] = {
 	{
 		.mmc		= 1,
@@ -468,6 +504,7 @@ static struct twl4030_platform_data omap3evm_twldata = {
 	.gpio		= &omap3evm_gpio_data,
 	.vdac		= &omap3_evm_vdac,
 	.vpll2		= &omap3_evm_vpll2,
+	.vaux2		= &omap3evm_vaux2,
 };
 
 static struct i2c_board_info __initdata omap3evm_i2c_boardinfo[] = {
@@ -551,6 +588,7 @@ static void __init omap3_evm_init_irq(void)
 static struct platform_device *omap3_evm_devices[] __initdata = {
 	&omap3_evm_dss_device,
 	&omap3evm_smc911x_device,
+	&omap3evm_camkit_device,
 };
 
 static struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
@@ -617,6 +655,9 @@ static void __init omap3_evm_init(void)
 	usb_musb_init();
 	usb_ehci_init(&ehci_pdata);
 	ads7846_dev_init();
+
+	omap3evmdc_init(0, 3, 0x5D);
+
 	omap3_evm_display_init();
 }
 
