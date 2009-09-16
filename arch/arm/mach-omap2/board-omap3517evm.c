6ad2512a13798f633f10477867f9a8e4f7e7ff47
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-09-16 16:43:53 +0530

    ehci: adding support for OMAP3517 EHCI (changes in board-omap3517evm.c)

--- arch/arm/mach-omap2/board-omap3517evm.c
+++ arch/arm/mach-omap2/board-omap3517evm.c
@@ -99,6 +99,19 @@ static void __init omap3517_evm_init_irq(void)
 	omap_gpio_init();
 }
 
+static struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
+
+	.port_mode[0] = EHCI_HCD_OMAP_MODE_PHY,
+	.port_mode[1] = EHCI_HCD_OMAP_MODE_PHY,
+	.port_mode[2] = EHCI_HCD_OMAP_MODE_UNKNOWN,
+
+	.chargepump = false,
+	.phy_reset  = true,
+	.reset_gpio_port[0]  = 57,
+	.reset_gpio_port[1]  = -EINVAL,
+	.reset_gpio_port[2]  = -EINVAL
+};
+
 static void __init omap3517_evm_init(void)
 {
 	omap3517_evm_i2c_init();
@@ -110,6 +123,9 @@ static void __init omap3517_evm_init(void)
 	omap3517_evm_ethernet_init();
 
 	usb_musb_init();
+	/* Setup EHCI phy reset padconfig for port1 using GPIO57 */
+	omap_cfg_reg(N5_3517_GPIO57_OUT);
+	usb_ehci_init(&ehci_pdata);
 }
 
 static void __init omap3517_evm_map_io(void)
