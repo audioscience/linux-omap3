35e7ab7105b125dd1d4ae9e351cd0c0a53197d80
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-09-22 12:57:08 +0530

    3517: musb: kill SECTION_MISMATCH warnings

--- drivers/usb/musb/cppi41.c
+++ drivers/usb/musb/cppi41.c
@@ -60,7 +60,7 @@ static struct {
 
 /******************** CPPI 4.1 Functions (External Interface) *****************/
 
-int __init cppi41_queue_mgr_init(u8 q_mgr, dma_addr_t rgn0_base, u16 rgn0_size)
+int cppi41_queue_mgr_init(u8 q_mgr, dma_addr_t rgn0_base, u16 rgn0_size)
 {
 	void __iomem *q_mgr_regs;
 	void *ptr;
@@ -134,7 +134,7 @@ int cppi41_dma_sched_tbl_init(u8 dma_num, u8 q_mgr,
 }
 EXPORT_SYMBOL(cppi41_dma_sched_tbl_init);
 
-int __init cppi41_dma_block_init(u8 dma_num, u8 q_mgr, u8 num_order,
+int cppi41_dma_block_init(u8 dma_num, u8 q_mgr, u8 num_order,
 				 u32 *sched_tbl, u8 tbl_size)
 {
 	const struct cppi41_dma_block *dma_block;
