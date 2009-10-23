kernelzilla/android-kernel

OMAP3: PM: misc. compile fixes post-2.6.29
Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

arch/arm/mach-omap2/pm.c
@@ -125,6 +125,7 @@ static ssize_t idle_store(struct kobject *kobj, struct kobj_attribute *attr,
 }
 
 #ifdef CONFIG_OMAP_PM_SRF
+#include <mach/omap34xx.h>
 static int vdd1_locked;
 static int vdd2_locked;
 

audioscience/linux-omap3

arch/arm/mach-omap2/pm.c
@@ -80,6 +80,7 @@ static ssize_t idle_store(struct kobject *kobj, struct kobj_attribute *attr,
 }
 
 #ifdef CONFIG_OMAP_PM_SRF
+#include <plat/omap34xx.h>
 static int vdd1_locked;
 static int vdd2_locked;
 