kernelzilla/android-kernel

OMAP3: PM: Fix compile error with 'CONFIG_OMAP_PM_SRF=y'
This patch fixes below compile error. The header file
'mach/omap34xx.h' declared all needed symbols.

  CC      arch/arm/mach-omap2/pm.o
  CC      arch/arm/mach-omap2/resource34xx.o
arch/arm/mach-omap2/resource34xx.c: In function 'resource_access_opp_lock':
arch/arm/mach-omap2/resource34xx.c:174: error: 'VDD1_OPP' undeclared
(first use in this function)
arch/arm/mach-omap2/resource34xx.c:177: error: 'VDD2_OPP' undeclared
(first use in this function)
arch/arm/mach-omap2/resource34xx.c:209: error: 'MAX_VDD1_OPP'
undeclared (first use in this function)
<snip>
make[1]: *** [arch/arm/mach-omap2/resource34xx.o] Error 1
arch/arm/mach-omap2/pm.c: In function 'vdd_opp_show':
arch/arm/mach-omap2/pm.c:140: error: 'VDD1_OPP' undeclared (first use
in this function)
arch/arm/mach-omap2/pm.c:140: error: (Each undeclared identifier is
reported only once
<snip>
make[1]: *** [arch/arm/mach-omap2/pm.o] Error 1

Signed-off-by: Kim Kyuwon <q1.kim@samsung.com>

arch/arm/mach-omap2/pm.c
@@ -34,6 +34,7 @@
 #include <mach/powerdomain.h>
 #include <mach/omapdev.h>
 #include <mach/resource.h>
+#include <mach/omap34xx.h>
 
 #include "prm-regbits-34xx.h"
 #include "pm.h"

arch/arm/mach-omap2/resource34xx.c
@@ -20,6 +20,7 @@
 #include <linux/cpufreq.h>
 #include <mach/powerdomain.h>
 #include <mach/clockdomain.h>
+#include <mach/omap34xx.h>
 #include "smartreflex.h"
 #include "resource34xx.h"
 #include "pm.h"

audioscience/linux-omap3

arch/arm/mach-omap2/pm.c
@@ -33,6 +33,7 @@
 #include <plat/powerdomain.h>
 #include <plat/omapdev.h>
 #include <plat/resource.h>
+#include <plat/omap34xx.h>
 
 #include "prm-regbits-34xx.h"
 #include "pm.h"

arch/arm/mach-omap2/resource34xx.c
@@ -21,6 +21,7 @@
 
 #include <plat/powerdomain.h>
 #include <plat/clockdomain.h>
+#include <plat/omap34xx.h>
 
 #include "smartreflex.h"
 #include "resource34xx.h"