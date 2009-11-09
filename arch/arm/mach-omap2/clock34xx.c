8cacaffbb82f9d203d35bd309c32364369896a28
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-11-09 18:44:31 +0530

    OMAP3: Common mechanism to identify cpu revision
    There are multiple mechanisms to identify the cpu revisions.
    Most common is use of omap_rev(). This, however, does a
    absolute comparison of omap_revision - which includes
    CPU id, CPU rev and CPU class. This comparison fails for
    OMAP35x processors.
    
    This patch defines generic functions that use only the
    CPU rev bits in omap_revision to identify the revision
    information.
    
    Usage will change from (for example):
            if (omap_rev() > OMAP3430_REV_ES2_0)
    to:
            if (cpu_is_omap34xx() && omap_rev_gt_2_0())
    
    Specific check for cpu_is_xxx() will not be needed for
    files specific to silicon e.g. pm34xx.c, clock34xx.c, etc.

--- arch/arm/mach-omap2/clock34xx.c
+++ arch/arm/mach-omap2/clock34xx.c
@@ -822,7 +822,7 @@ static int omap3_dpll4_set_rate(struct clk *clk, unsigned long rate)
 	 * on 3430ES1 prevents us from changing DPLL multipliers or dividers
 	 * on DPLL4.
 	 */
-	if (omap_rev() == OMAP3430_REV_ES1_0) {
+	if (omap_rev_is_1_0()) {
 		printk(KERN_ERR "clock: DPLL4 cannot change rate due to "
 		       "silicon 'Limitation 2.5' on 3430ES1.\n");
 		return -EINVAL;
@@ -1184,7 +1184,7 @@ int __init omap2_clk_init(void)
 		 * Update this if there are further clock changes between ES2
 		 * and production parts
 		 */
-		if (omap_rev() == OMAP3430_REV_ES1_0) {
+		if (omap_rev_is_1_0()) {
 			/* No 3430ES1-only rates exist, so no RATE_IN_3430ES1 */
 			cpu_clkflg |= CK_3430ES1;
 		} else {
