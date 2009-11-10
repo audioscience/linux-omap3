fc052502ae71ce2ae2927a85d2027b76cc2ab940
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-11-10 14:01:52 +0530

    omap3evm: Enable GPIO latch chip using T2_GPIO6
    This has to be done to enable U131 chip used to latch USB, Camera and
    Audio lines.
    
    Should be done in generic way at board files.
    
    Revert once generic implementation completed.

--- drivers/mfd/twl4030-core.c
+++ drivers/mfd/twl4030-core.c
@@ -37,6 +37,7 @@
 
 #include <linux/i2c.h>
 #include <linux/i2c/twl4030.h>
+#include <plat/board.h>
 
 #if defined(CONFIG_ARCH_OMAP2) || defined(CONFIG_ARCH_OMAP3)
 #include <plat/cpu.h>
@@ -762,6 +763,31 @@ static int twl4030_remove(struct i2c_client *client)
 	return 0;
 }
 
+#define TWL4030_VAUX2_1P8V 0x5
+#define ENABLE_VAUX2_DEV_GRP 0x20
+
+/* Enable USB GPIOs on new OMAP3EVM */
+static void usb_gpio_settings(void)
+{
+	unsigned char val;
+
+	if (get_omap3_evm_rev() < OMAP3EVM_BOARD_GEN_2)
+		return;
+
+	/* Enable TWL GPIO Module */
+	twl4030_i2c_write_u8(TWL4030_MODULE_GPIO, 0x04, REG_GPIO_CTRL);
+
+	/* Configure GPIO-6 as output */
+	twl4030_i2c_read_u8(TWL4030_MODULE_GPIO, &val, REG_GPIODATADIR1);
+	val |= 0x4;
+	twl4030_i2c_write_u8(TWL4030_MODULE_GPIO, val, REG_GPIODATADIR1);
+
+	/* Set GPIO6 = 1 */
+	twl4030_i2c_read_u8(TWL4030_MODULE_GPIO, &val, REG_GPIODATAOUT1);
+	val |= 0x40;
+	twl4030_i2c_write_u8(TWL4030_MODULE_GPIO, val, REG_GPIODATAOUT1);
+}
+
 /* NOTE:  this driver only handles a single twl4030/tps659x0 chip */
 static int
 twl4030_probe(struct i2c_client *client, const struct i2c_device_id *id)
@@ -824,6 +850,8 @@ twl4030_probe(struct i2c_client *client, const struct i2c_device_id *id)
 	}
 
 	status = add_children(pdata, id->driver_data);
+
+	usb_gpio_settings();
 fail:
 	if (status < 0)
 		twl4030_remove(client);
