4279d776e0f05d8fb2bc51f0c50a5189c9513f50
Sanjeev Premi <premi@ti.com>
2009-11-18 20:37:39 +0530

    Fix compilation warnings
    This patch fixes these complier warnings that were caused
    due to inclusion of header file (omap3-opp.h) which defines
    static arrays.
    
      CC      arch/arm/mach-omap2/clock34xx.o
    arch/arm/mach-omap2/omap3-opp.h:26: warning: 'omap3_mpu_rate
    _table' defined but not used
    arch/arm/mach-omap2/omap3-opp.h:42: warning: 'omap3_l3_rate_
    table' defined but not used
    arch/arm/mach-omap2/omap3-opp.h:52: warning: 'omap3_dsp_rate
    _table' defined but not used
    
    The array definition has been moved to appropriate C file and
    the header declares the arrays as extern.
    
      [1] SDOCM00062536

--- arch/arm/mach-omap2/clock34xx.c
+++ arch/arm/mach-omap2/clock34xx.c
@@ -332,6 +332,48 @@ static struct omap_clk omap34xx_clks[] = {
 	CLK(NULL, "uart4_ick", &uart4_ick, CK_35XX),
 };
 
+struct omap_opp omap3_mpu_rate_table[] = {
+	{0, 0, 0},
+	/*OPP1*/
+	{S125M, VDD1_OPP1, 0x1E},
+	/*OPP2*/
+	{S250M, VDD1_OPP2, 0x26},
+	/*OPP3*/
+	{S500M, VDD1_OPP3, 0x30},
+	/*OPP4*/
+	{S550M, VDD1_OPP4, 0x36},
+	/*OPP5*/
+	{S600M, VDD1_OPP5, 0x3C},
+	/*OPP6*/
+	{S720M, VDD1_OPP6, 0x3C},
+};
+
+struct omap_opp omap3_l3_rate_table[] = {
+	{0, 0, 0},
+	/*OPP1*/
+	{0, VDD2_OPP1, 0x1E},
+	/*OPP2*/
+	{S83M, VDD2_OPP2, 0x24},
+	/*OPP3*/
+	{S166M, VDD2_OPP3, 0x2C},
+};
+
+struct omap_opp omap3_dsp_rate_table[] = {
+	{0, 0, 0},
+	/*OPP1*/
+	{S90M, VDD1_OPP1, 0x1E},
+	/*OPP2*/
+	{S180M, VDD1_OPP2, 0x26},
+	/*OPP3*/
+	{S360M, VDD1_OPP3, 0x30},
+	/*OPP4*/
+	{S400M, VDD1_OPP4, 0x36},
+	/*OPP5*/
+	{S430M, VDD1_OPP5, 0x3C},
+	/*OPP5*/
+	{S520M, VDD1_OPP6, 0x3C},
+};
+
 /* CM_AUTOIDLE_PLL*.AUTO_* bit values */
 #define DPLL_AUTOIDLE_DISABLE			0x0
 #define DPLL_AUTOIDLE_LOW_POWER_STOP		0x1
