2397af767a2bf2617b3a3aea98d4c26a3fc66d2b
Anuj Aggarwal <anuj.aggarwal@ti.com>
2009-11-20 12:18:27 +0530

    Cust: OMAP: McBSP: Adding functions to enable/disable mcbsp clocks
    Currently, McBSP clocks are enabled while requesting the device and
    disabled while freeing the same. New APIs are added so that the
    clocks can be released/acquired as and when required, specifically
    for suspend/resume operations.

--- arch/arm/plat-omap/include/plat/mcbsp.h
+++ arch/arm/plat-omap/include/plat/mcbsp.h
@@ -440,6 +440,8 @@ static inline int omap_mcbsp_get_dma_op_mode(unsigned int id) { return 0; }
 #endif
 int omap_mcbsp_request(unsigned int id);
 void omap_mcbsp_free(unsigned int id);
+void omap_mcbsp_disable_clks(unsigned int id);
+void omap_mcbsp_enable_clks(unsigned int id);
 void omap_mcbsp_start(unsigned int id, int tx, int rx);
 void omap_mcbsp_stop(unsigned int id, int tx, int rx);
 void omap_mcbsp_xmit_word(unsigned int id, u32 word);
--- arch/arm/plat-omap/mcbsp.c
+++ arch/arm/plat-omap/mcbsp.c
@@ -459,6 +459,35 @@ int omap_mcbsp_request(unsigned int id)
 }
 EXPORT_SYMBOL(omap_mcbsp_request);
 
+void omap_mcbsp_disable_clks(unsigned int id)
+{
+	struct omap_mcbsp *mcbsp;
+
+	if (!omap_mcbsp_check_valid_id(id)) {
+		printk(KERN_ERR "%s: Invalid id (%d)\n", __func__, id + 1);
+		return;
+	}
+	mcbsp = id_to_mcbsp_ptr(id);
+	clk_disable(mcbsp->fclk);
+	clk_disable(mcbsp->iclk);
+}
+EXPORT_SYMBOL(omap_mcbsp_disable_clks);
+
+
+void omap_mcbsp_enable_clks(unsigned int id)
+{
+	struct omap_mcbsp *mcbsp;
+
+	if (!omap_mcbsp_check_valid_id(id)) {
+		printk(KERN_ERR "%s: Invalid id (%d)\n", __func__, id + 1);
+		return;
+	}
+	mcbsp = id_to_mcbsp_ptr(id);
+	clk_enable(mcbsp->iclk);
+	clk_enable(mcbsp->fclk);
+}
+EXPORT_SYMBOL(omap_mcbsp_enable_clks);
+
 void omap_mcbsp_free(unsigned int id)
 {
 	struct omap_mcbsp *mcbsp;
