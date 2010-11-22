d7fdd057f3f0f3afa296518d265fcef94c38d8cc
Ajay Kumar Gupta <ajay.gupta@ti.com>
2010-11-22 16:53:26 +0530

    musb: Add back old musb procfs file
    Procfs file is removed from musb and an equivalent debugfs is
    expected to replace it.
    
    Adding the old file back to retain the debug functionalities untill
    we get debugfs equivalent in mainline.
    
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2140,6 +2140,9 @@ bad_config:
 			? "DMA" : "PIO",
 			musb->nIrq);
 
+	if (status == 0)
+		musb_debug_create("driver/musb_hdrc", musb);
+
 	return 0;
 
 fail5:
@@ -2219,6 +2222,7 @@ static int __exit musb_remove(struct platform_device *pdev)
 	 */
 	musb_exit_debugfs(musb);
 	musb_shutdown(pdev);
+	musb_debug_delete("driver/musb_hdrc", musb);
 
 	musb_free(musb);
 	iounmap(ctrl_base);
