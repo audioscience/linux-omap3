1641fbfbdf7a4c3f80c293ba7e1dcba538eba918
Hemant Pedanekar <hemantp@ti.com>
2010-11-26 15:08:25 +0530

    TI814X: Add minimal hwmod support
    This patch updates exising TI816X hwmods to support TI814X.
    
    Note that only minimum required hwmods for booting on TI814X are currently
    added.
    
    Signed-off-by: Hemant Pedanekar <hemantp@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_81xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_81xx_data.c
@@ -70,7 +70,7 @@ static struct omap_hwmod ti816x_l3_slow_hwmod = {
 	.masters_cnt	= ARRAY_SIZE(ti816x_l3_slow_masters),
 	.slaves		= ti816x_l3_slow_slaves,
 	.slaves_cnt	= ARRAY_SIZE(ti816x_l3_slow_slaves),
-	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X | CHIP_IS_TI814X),
 	.flags		= HWMOD_NO_IDLEST,
 };
 
@@ -152,7 +152,7 @@ static struct omap_hwmod ti816x_l4_slow_hwmod = {
 	.masters_cnt	= ARRAY_SIZE(ti816x_l4_slow_masters),
 	.slaves		= ti816x_l4_slow_slaves,
 	.slaves_cnt	= ARRAY_SIZE(ti816x_l4_slow_slaves),
-	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X | CHIP_IS_TI814X),
 	.flags		= HWMOD_NO_IDLEST,
 };
 
@@ -168,7 +168,7 @@ static struct omap_hwmod ti816x_mpu_hwmod = {
 	.main_clk	= "mpu_ck",
 	.masters	= ti816x_mpu_masters,
 	.masters_cnt	= ARRAY_SIZE(ti816x_mpu_masters),
-	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X | CHIP_IS_TI814X),
 };
 
 /* UART common */
@@ -225,7 +225,7 @@ static struct omap_hwmod ti816x_uart1_hwmod = {
 	.slaves		= ti816x_uart1_slaves,
 	.slaves_cnt	= ARRAY_SIZE(ti816x_uart1_slaves),
 	.class		= &uart_class,
-	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X | CHIP_IS_TI814X),
 };
 
 /* UART2 */
@@ -258,7 +258,7 @@ static struct omap_hwmod ti816x_uart2_hwmod = {
 	.slaves		= ti816x_uart2_slaves,
 	.slaves_cnt	= ARRAY_SIZE(ti816x_uart2_slaves),
 	.class		= &uart_class,
-	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X | CHIP_IS_TI814X),
 };
 
 /* UART3 */
@@ -291,7 +291,7 @@ static struct omap_hwmod ti816x_uart3_hwmod = {
 	.slaves		= ti816x_uart3_slaves,
 	.slaves_cnt	= ARRAY_SIZE(ti816x_uart3_slaves),
 	.class		= &uart_class,
-	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X | CHIP_IS_TI814X),
 };
 
 static __initdata struct omap_hwmod *ti81xx_hwmods[] = {
