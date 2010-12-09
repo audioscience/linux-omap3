66438be56e6cdfffb922d18bbe53b8e92f2ca970
Ravi B <ravibabu@ti.com>
2010-12-09 17:59:46 +0530

    usb: Adding usb-musb support for ti81xx platform
    This patch adds the usb platform support for
            ti81xx platform.
            Adds two instances of musb controller for
            ti81xx platform in usb-musb.c
    
    Signed-off-by: Ravi B <ravibabu@ti.com>

--- arch/arm/mach-omap2/usb-musb.c
+++ arch/arm/mach-omap2/usb-musb.c
@@ -102,6 +102,11 @@ static struct resource musb_resources[] = {
 		.flags	= IORESOURCE_IRQ,
 	},
 	[2] = {	/* DMA IRQ */
+#ifdef CONFIG_ARCH_TI81XX
+		.flags  = IORESOURCE_MEM,
+	},
+	[3] = { /* DMA IRQ */
+#endif
 		.start	= INT_243X_HS_USB_DMA,
 		.flags	= IORESOURCE_IRQ,
 	},
@@ -115,67 +120,86 @@ static struct musb_hdrc_config musb_config = {
 	.ram_bits	= 12,
 };
 
-static struct musb_hdrc_platform_data musb_plat = {
-#ifdef CONFIG_USB_MUSB_OTG
-	.mode		= MUSB_OTG,
-#elif defined(CONFIG_USB_MUSB_HDRC_HCD)
-	.mode		= MUSB_HOST,
-#elif defined(CONFIG_USB_GADGET_MUSB_HDRC)
-	.mode		= MUSB_PERIPHERAL,
-#endif
-	/* .clock is set dynamically */
-	.config		= &musb_config,
-
-	/* REVISIT charge pump on TWL4030 can supply up to
-	 * 100 mA ... but this value is board-specific, like
-	 * "mode", and should be passed to usb_musb_init().
-	 */
-	.power		= 50,			/* up to 100 mA */
+static struct musb_hdrc_platform_data musb_plat[] = {
+	{
+		.config         = &musb_config,
+		.clock          = "ick",
+	},
+	{
+		.config         = &musb_config,
+		.clock          = "ick",
+	},
 };
 
 static u64 musb_dmamask = DMA_BIT_MASK(32);
 
-static struct platform_device musb_device = {
-	.name		= "musb-omap2430",
-	.id		= -1,
-	.dev = {
-		.dma_mask		= &musb_dmamask,
-		.coherent_dma_mask	= DMA_BIT_MASK(32),
-		.platform_data		= &musb_plat,
+static struct platform_device musb_device[] = {
+	{
+		.name		= "musb-omap2430",
+		.id		= 0,
+		.dev = {
+			.dma_mask		= &musb_dmamask,
+			.coherent_dma_mask	= DMA_BIT_MASK(32),
+			.platform_data		= &musb_plat[0],
+		},
+		.num_resources	= ARRAY_SIZE(musb_resources)/2,
+		.resource	= &musb_resources[0],
 	},
-	.num_resources	= ARRAY_SIZE(musb_resources),
-	.resource	= musb_resources,
+	{
+		.name		= "musb-omap2430",
+		.id		= 1,
+		.dev = {
+			.dma_mask		= &musb_dmamask,
+			.coherent_dma_mask	= DMA_BIT_MASK(32),
+			.platform_data		= &musb_plat[1],
+		},
+		.num_resources	= ARRAY_SIZE(musb_resources)/2,
+		.resource	= &musb_resources[2],
+	},
+
 };
 
 void __init usb_musb_init(struct omap_musb_board_data *board_data)
 {
+	int i;
+
+	musb_resources[0].end = musb_resources[0].start + SZ_4K - 1;
+
 	if (cpu_is_omap243x()) {
 		musb_resources[0].start = OMAP243X_HS_BASE;
 	} else if (cpu_is_omap3517() || cpu_is_omap3505()) {
-		musb_device.name = "musb-am35x";
+		musb_device[0].name = "musb-am35x";
 		musb_resources[0].start = AM35XX_IPSS_USBOTGSS_BASE;
 		musb_resources[1].start = INT_35XX_USBOTG_IRQ;
+		musb_resources[0].end = musb_resources[0].start + SZ_32K - 1;
 	} else if (cpu_is_omap34xx()) {
 		musb_resources[0].start = OMAP34XX_HSUSB_OTG_BASE;
 	} else if (cpu_is_omap44xx()) {
 		musb_resources[0].start = OMAP44XX_HSUSB_OTG_BASE;
 		musb_resources[1].start = OMAP44XX_IRQ_HS_USB_MC_N;
 		musb_resources[2].start = OMAP44XX_IRQ_HS_USB_DMA_N;
-	}
-	if (cpu_is_omap3517())
-		musb_resources[0].end = musb_resources[0].start + SZ_32K - 1;
-	else
-		musb_resources[0].end = musb_resources[0].start + SZ_4K - 1;
+	} else if (cpu_is_ti81xx()) {
 
-	/*
-	 * REVISIT: This line can be removed once all the platforms using
-	 * musb_core.c have been converted to use use clkdev.
-	 */
-	musb_plat.clock = "ick";
-	musb_plat.board_data = board_data;
-	musb_plat.power = board_data->power >> 1;
-	musb_plat.mode = board_data->mode;
-	musb_plat.extvbus = board_data->extvbus;
+		/* disable musb multipoint support for ti8168 */
+		if (cpu_is_ti816x())
+			musb_config.multipoint = 0;
+
+		/* only usb0 port enabled in peripheral mode*/
+		if (board_data->mode == MUSB_PERIPHERAL)
+			board_data->instances = 0;
+
+		musb_resources[0].start = TI81XX_USB0_BASE;
+		musb_resources[1].start = TI81XX_IRQ_USB0;
+		musb_resources[0].end = musb_resources[0].start + SZ_2K - 1;
+
+		for (i = 0; i <= board_data->instances; i++)
+			musb_device[i].name = "musb-ti81xx";
+
+		musb_resources[2].start = TI81XX_USB1_BASE;
+		musb_resources[3].start = TI81XX_IRQ_USB1;
+		musb_resources[2].end = musb_resources[2].start + SZ_2K - 1;
+		musb_config.fifo_mode = 4;
+	}
 
 	/*
 	 * OMAP3630/AM35x platform has MUSB RTL-1.8 which has the fix for the
@@ -185,8 +209,20 @@ void __init usb_musb_init(struct omap_musb_board_data *board_data)
 	if (cpu_is_omap3430())
 		musb_config.fifo_mode = 5;
 
-	if (platform_device_register(&musb_device) < 0)
-		printk(KERN_ERR "Unable to register HS-USB (MUSB) device\n");
+	for (i = 0; i <= board_data->instances; i++) {
+		if (cpu_is_ti816x())
+			musb_plat[i].clock = "usbotg_ick";
+		else if (cpu_is_ti814x())
+			musb_plat[i].clock = "usb_ick";
+
+		musb_plat[i].board_data = board_data;
+		musb_plat[i].power = board_data->power >> 1;
+		musb_plat[i].mode = board_data->mode;
+		musb_plat[i].extvbus = board_data->extvbus;
+
+		if (platform_device_register(&musb_device[i]) < 0)
+			printk(KERN_ERR "Unable to register HS-USB (MUSB) device\n");
+	}
 
 	usb_musb_pm_init();
 }
