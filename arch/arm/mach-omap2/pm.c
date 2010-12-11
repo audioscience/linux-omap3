f540b741a3095b7b0cf4d2a3d4a31fb81e5f4066
Hemant Pedanekar <hemantp@ti.com>
2010-12-11 08:08:03 +0530

    TI81XX: Initialize only applicable processor devices
    This patch avoids initialising devices which do not have hwmods added for TI81XX
    - e.g., "iva". Also adds TI81XX specific device initialization - e.g.,
    initialize "l3_slow" instead of "l3_main".
    
    These avoid warning messages during the boot such as:
    
    ------------[ cut here ]------------
    WARNING: at arch/arm/mach-omap2/pm.c:64 _init_omap_device+0x90/0xb0()
    _init_omap_device: could not find omap_hwmod for iva
    
    ------------[ cut here ]------------
    WARNING: at arch/arm/mach-omap2/pm.c:64 _init_omap_device+0x90/0xb0()
    _init_omap_device: could not find omap_hwmod for l3_main
    
    Signed-off-by: Hemant Pedanekar <hemantp@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -80,10 +80,15 @@ static int _init_omap_device(char *name, struct device **new_dev)
 static void omap2_init_processor_devices(void)
 {
 	_init_omap_device("mpu", &mpu_dev);
-	_init_omap_device("iva", &iva_dev);
+
+	if (!cpu_is_ti81xx())
+		_init_omap_device("iva", &iva_dev);
+
 	if (cpu_is_omap44xx()) {
 		_init_omap_device("l3_main_1", &l3_dev);
 		_init_omap_device("dsp", &dsp_dev);
+	} else if (cpu_is_ti81xx()) {
+		_init_omap_device("l3_slow", &l3_dev);
 	} else {
 		_init_omap_device("l3_main", &l3_dev);
 	}
