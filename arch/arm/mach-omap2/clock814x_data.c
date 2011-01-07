1017802dfdcea29f4520fa83e811ada3d4ca0d0b
Anil Kumar Ch <anilkumar@ti.com>
2011-01-07 20:19:33 +0530

    ti814x: Changes the instances of mcspi
    This patch changes the instances of mcsapi from 0, 1, 2, 3
    to 1, 2, 3, 4
    
    Signed-off-by: Anil Kumar Ch <anilkumar@ti.com>

--- arch/arm/mach-omap2/clock814x_data.c
+++ arch/arm/mach-omap2/clock814x_data.c
@@ -939,15 +939,6 @@ static struct clk i2c4_ick = {
 };
 
 /* McSPI0 Clock(final) */
-static struct clk mcspi0_ick = {
-	.name		= "mcspi0_ick",
-	.parent		= &sysclk6_ck,
-	.ops		= &clkops_null,
-	.clkdm_name	= "alwon_l3_slow_clkdm",
-	.recalc		= &followparent_recalc,
-};
-
-/* McSPI1 Clock(final) */
 static struct clk mcspi1_ick = {
 	.name		= "mcspi1_ick",
 	.parent		= &sysclk6_ck,
@@ -956,7 +947,7 @@ static struct clk mcspi1_ick = {
 	.recalc		= &followparent_recalc,
 };
 
-/* McSPI2 Clock(final) */
+/* McSPI1 Clock(final) */
 static struct clk mcspi2_ick = {
 	.name		= "mcspi2_ick",
 	.parent		= &sysclk6_ck,
@@ -965,9 +956,18 @@ static struct clk mcspi2_ick = {
 	.recalc		= &followparent_recalc,
 };
 
-/* McSPI3 Clock(final) */
+/* McSPI2 Clock(final) */
 static struct clk mcspi3_ick = {
 	.name		= "mcspi3_ick",
+	.parent		= &sysclk6_ck,
+	.ops		= &clkops_null,
+	.clkdm_name	= "alwon_l3_slow_clkdm",
+	.recalc		= &followparent_recalc,
+};
+
+/* McSPI3 Clock(final) */
+static struct clk mcspi4_ick = {
+	.name		= "mcspi4_ick",
 	.parent		= &sysclk6_ck,
 	.ops		= &clkops_null,
 	.clkdm_name	= "alwon_l3_slow_clkdm",
@@ -1575,17 +1575,6 @@ static struct clk sysclk10_ck = {
 };
 
 /* SPI0 Functional Clock(final) */
-static struct clk mcspi0_fck = {
-	.name		= "mcspi0_fck",
-	.parent		= &sysclk10_ck,
-	.ops		= &clkops_omap2_dflt,
-	.enable_reg	= TI81XX_CM_ALWON_SPI_CLKCTRL,
-	.enable_bit	= TI81XX_MODULEMODE_SWCTRL,
-	.clkdm_name	= "alwon_l3_slow_clkdm",
-	.recalc		= &followparent_recalc,
-};
-
-/* SPI1 Functional Clock(final) */
 static struct clk mcspi1_fck = {
 	.name		= "mcspi1_fck",
 	.parent		= &sysclk10_ck,
@@ -1596,7 +1585,7 @@ static struct clk mcspi1_fck = {
 	.recalc		= &followparent_recalc,
 };
 
-/* SPI2 Functional Clock(final) */
+/* SPI1 Functional Clock(final) */
 static struct clk mcspi2_fck = {
 	.name		= "mcspi2_fck",
 	.parent		= &sysclk10_ck,
@@ -1607,9 +1596,20 @@ static struct clk mcspi2_fck = {
 	.recalc		= &followparent_recalc,
 };
 
-/* SPI3 Functional Clock(final) */
+/* SPI2 Functional Clock(final) */
 static struct clk mcspi3_fck = {
 	.name		= "mcspi3_fck",
+	.parent		= &sysclk10_ck,
+	.ops		= &clkops_omap2_dflt,
+	.enable_reg	= TI81XX_CM_ALWON_SPI_CLKCTRL,
+	.enable_bit	= TI81XX_MODULEMODE_SWCTRL,
+	.clkdm_name	= "alwon_l3_slow_clkdm",
+	.recalc		= &followparent_recalc,
+};
+
+/* SPI3 Functional Clock(final) */
+static struct clk mcspi4_fck = {
+	.name		= "mcspi4_fck",
 	.parent		= &sysclk10_ck,
 	.ops		= &clkops_omap2_dflt,
 	.enable_reg	= TI81XX_CM_ALWON_SPI_CLKCTRL,
@@ -3268,10 +3268,10 @@ static struct omap_clk ti814x_clks[] = {
 	CLK("i2c_omap.2",	"ick",				&i2c2_ick,			CK_TI814X),
 	CLK("i2c_omap.3",	"ick",				&i2c3_ick,			CK_TI814X),
 	CLK("i2c_omap.4",	"ick",				&i2c4_ick,			CK_TI814X),
-	CLK("omap2_mcspi.0",	"ick",				&mcspi0_ick,			CK_TI814X),
 	CLK("omap2_mcspi.1",	"ick",				&mcspi1_ick,			CK_TI814X),
 	CLK("omap2_mcspi.2",	"ick",				&mcspi2_ick,			CK_TI814X),
 	CLK("omap2_mcspi.3",	"ick",				&mcspi3_ick,			CK_TI814X),
+	CLK("omap2_mcspi.4",	"ick",				&mcspi4_ick,			CK_TI814X),
 	CLK(NULL,		"sdio_ick",			&sdio_ick,			CK_TI814X),
 	CLK(NULL,		"gpt1_ick",			&gpt1_ick,			CK_TI814X),
 	CLK(NULL,		"gpt2_ick",			&gpt2_ick,			CK_TI814X),
@@ -3334,10 +3334,10 @@ static struct omap_clk ti814x_clks[] = {
 	CLK(NULL,		"usb_phy1_rclk_ick",		&usb_phy1_rclk_ick,		CK_TI814X),
 	CLK(NULL,		"usb_dpll_div5_ck",		&usb_dpll_div5_ck,		CK_TI814X),
 	CLK(NULL,		"sysclk10_ck",			&sysclk10_ck,			CK_TI814X),
-	CLK("omap2_mcspi.0",	"fck",				&mcspi0_fck,			CK_TI814X),
 	CLK("omap2_mcspi.1",	"fck",				&mcspi1_fck,			CK_TI814X),
 	CLK("omap2_mcspi.2",	"fck",				&mcspi2_fck,			CK_TI814X),
 	CLK("omap2_mcspi.3",	"fck",				&mcspi3_fck,			CK_TI814X),
+	CLK("omap2_mcspi.4",	"fck",				&mcspi4_fck,			CK_TI814X),
 	CLK(NULL,		"i2c02_ck",			&i2c02_ck,			CK_TI814X),
 	CLK(NULL,		"i2c13_ck",			&i2c13_ck,			CK_TI814X),
 	CLK("i2c_omap.1",	"fck",				&i2c1_fck,			CK_TI814X),
