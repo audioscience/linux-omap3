mturquette/linux-omap

OMAP: Disable smartreflex across DVFS
This patch disables smartreflex for a particular voltage
domain when the the voltage domain and the devices belonging
to it is being scaled and re-enables it back once the scaling
is done.

Signed-off-by: Thara Gopinath <thara@ti.com>

arch/arm/mach-omap2/voltage.c
@@ -35,6 +35,7 @@
 #include <plat/clock.h>
 #include <plat/common.h>
 #include <plat/voltage.h>
+#include <plat/smartreflex.h>
 
 #include "prm-regbits-34xx.h"
 #include "prm44xx.h"
@@ -1727,6 +1728,9 @@ int omap_voltage_scale(struct voltagedomain *voltdm, unsigned long volt)
 		return -EINVAL;
 	}
 
+	/* Disable smartreflex module across voltage and frequency scaling */
+	omap_smartreflex_disable(voltdm);
+
 	if (curr_volt == volt) {
 		is_volt_scaled = 1;
 	} else if (curr_volt < volt) {
@@ -1760,6 +1764,9 @@ int omap_voltage_scale(struct voltagedomain *voltdm, unsigned long volt)
 		omap_voltage_scale_vdd(voltdm, volt);
 
 	mutex_unlock(&vdd->scaling_mutex);
+
+	/* Enable Smartreflex module */
+	omap_smartreflex_enable(voltdm);
 
 	/* Scale dependent vdds */
 	scale_dep_vdd(vdd);

audioscience/linux-omap3

arch/arm/mach-omap2/voltage.c
@@ -32,6 +32,7 @@
 #include <plat/common.h>
 #include <plat/voltage.h>
 #include <plat/omap_device.h>
+#include <plat/smartreflex.h>
 
 #include "prm-regbits-34xx.h"
 #include "prm-regbits-44xx.h"
@@ -1806,6 +1807,9 @@ int omap_voltage_scale(struct voltagedomain *voltdm, unsigned long volt)
 		return -EINVAL;
 	}
 
+	/* Disable smartreflex module across voltage and frequency scaling */
+	omap_sr_disable(voltdm);
+
 	if (curr_volt == volt) {
 		is_volt_scaled = 1;
 	} else if (curr_volt < volt) {
@@ -1839,6 +1843,9 @@ int omap_voltage_scale(struct voltagedomain *voltdm, unsigned long volt)
 		omap_voltage_scale_vdd(voltdm, volt);
 
 	mutex_unlock(&vdd->scaling_mutex);
+
+	/* Enable Smartreflex module */
+	omap_sr_enable(voltdm);
 
 	/* Scale dependent vdds */
 	scale_dep_vdd(vdd);