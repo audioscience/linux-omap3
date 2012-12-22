6330870557935f4b78679fcbb16b52676bf943c7
Stephen <sgturner@audioscience.com>
2012-12-21 16:16:27 -0800

    ASI1230: Re-enable SPI flash on bus 1, cs0 for revB h/w.  Remove bus1, cs2 - we don't have this one.  Add bus2, cs1 (will be PGA2505).

--- arch/arm/mach-omap2/board-asi1230.c
+++ arch/arm/mach-omap2/board-asi1230.c
@@ -92,45 +92,41 @@ static struct omap2_hsmmc_info mmc[] = {
 	 },
 	{}			/* Terminator */
 };
-#if 0 /* Disable SPI Flash for revA HW */
 const struct flash_platform_data asi1230_spi_flash = {
 	.type = "m25p16",
 	.name = "spi_flash",
 	.parts = NULL,
 	.nr_parts = 0,
 };
-#endif
 struct spi_board_info __initdata asi1230_spi_slave_info[] = {
-#if 0 /* Disable SPI Flash for revA HW */
 	{
 	 .modalias = "m25p80",
 	 .platform_data = &asi1230_spi_flash,
 	 .irq = -1,
-	 .max_speed_hz = 48000000,	// max speed of TI814x SPI is 48MHz
+	 .max_speed_hz = 48000000,	/* max speed of TI814x SPI is 48MHz */
 	 .bus_num = 1,
 	 .chip_select = 0,
 	 },
-#endif
 	{
-	 .modalias = "spidev",
+	 .modalias = "spidev",		/* spare */
 	 .irq = -1,
 	 .max_speed_hz = 48000000,
 	 .bus_num = 1,
 	 .chip_select = 1,
 	 },
 	{
-	 .modalias = "spidev",
-	 .irq = -1,
-	 .max_speed_hz = 48000000,
-	 .bus_num = 1,
-	 .chip_select = 2,
-	 },
-	{
-	 .modalias = "spidev",
+	 .modalias = "spidev",		/* 256x64 OLED (ASI2600 only) */
 	 .irq = -1,
 	 .max_speed_hz = 48000000,
 	 .bus_num = 2,
 	 .chip_select = 0,
+	 },
+	{
+	 .modalias = "spidev",		/* PGA2505 (ASI2350 only) */
+	 .irq = -1,
+	 .max_speed_hz = 48000000,
+	 .bus_num = 2,
+	 .chip_select = 1,
 	 },
 };
 
