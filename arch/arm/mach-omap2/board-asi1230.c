c44ea97d2c42e1a47093ea98d18feb9db148ee93
asitom <teckert@audioscience.com>
2024-01-17 14:38:55 -0500

    asi1230: debugging phy fixup

--- arch/arm/mach-omap2/board-asi1230.c
+++ arch/arm/mach-omap2/board-asi1230.c
@@ -178,6 +178,8 @@ static int asi1230_vsc_phy_fixup(struct phy_device *phydev)
 {
 	unsigned int val;
 
+	printk(KERN_INFO "asi1230_vsc_phy_fixup() called.\n");
+
 	/* Enable RGMII RX/TX clock skew */
 	val = phy_read(phydev, PHY_VSC8601_EXCTRL1_REG);
 	val |= PHY_VSC8601_RXCLKSKEW;
@@ -201,11 +203,16 @@ static int asi1230_dp_phy_fixup(struct phy_device *phydev)
 {
 	unsigned int val;
 
+	printk(KERN_INFO "asi1230_dp_phy_fixup() called.\n");
+
 	/* LED0 should lite when link present and flash on TX/RX */
 	val = phy_read(phydev, PHY_DP83867_LEDCR1_REG);
+	printk(KERN_INFO "asi1230_dp_phy_fixup() read: %x\n", val);
 	val = (val & ~PHY_DP83867_LEDCR1_MASK) | PHY_DP83867_LEDCR1_VALUE;
+	printk(KERN_INFO "asi1230_dp_phy_fixup() write: %x\n", val);
 	phy_write(phydev, PHY_VSC8601_EXCTRL1_REG, val);
 	val = phy_read(phydev, PHY_DP83867_LEDCR1_REG);
+	printk(KERN_INFO "asi1230_dp_phy_fixup() verify: %x\n", val);
 	return 0;
 }
 
